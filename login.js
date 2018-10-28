a4b02291fda78cb8e4bfb376011205966d53ceba
jenkey2011 <jenkey2011@163.com>
2018-10-28 23:43:53 +0800

    login

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const querystring = require('querystring')
 const urlLib = require('url')
 
-let user = {}
+let users = {}
 
 let server = http.createServer((req, res) => {
   // 解析数据
@@ -16,14 +16,38 @@ let server = http.createServer((req, res) => {
     const url = obj.pathname
     const get = obj.query
     const post = querystring.parse(str)
-
     if (url === '/user') {
-
+      switch (get.act) {
+        case 'reg':
+          // 检查用户名是否存在
+          if (users[get.name]) {
+            res.write('{"ok": false, "msg": "此用户已存在"}')
+          } else {
+            users[get.name] = get.password
+            res.write('{"ok": true, "msg": "注册成功"}')
+          }
+          break
+        case 'login':
+          // 检查用户名是否存在 是否正确
+          if (users[get.name] === undefined) {
+            res.write('{"ok": false, "msg": "此用户不存在"}')
+          } else if (users[get.name] === get.password) {
+            res.write('{"ok": true, "msg": "登录成功"}')
+          } else {
+            res.write('{"ok": false, "msg": "用户名或密码不正确"}')
+          }
+          break
+        default:
+          res.write('{"ok": false, "msg": "未知的act"}')
+          break
+      }
+      console.log(users)
+      res.end()
     } else {
       let fileName = './www' + url
       fs.readFile(fileName, (err, data) => {
         if (err) {
-          res.write(err)
+          res.write('404')
         } else {
           res.write(data)
         }
@@ -31,6 +55,6 @@ let server = http.createServer((req, res) => {
       })
     }
   })
-  res.write('hello word')
-  res.end()
-}).listen(8999)
+}).listen(8999, 'localhost', function () {
+
+})
